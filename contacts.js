393b664f21571cadea3da1a2c11fee23da0a1370
Ivan Nodyanosh <111501255+IvaNodyanosh@users.noreply.github.com>
2024-03-22 17:45:37 +0200

    complety task

---
+++ contacts.js
@@ -0,0 +1,47 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+
+  return result || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  let contact = null;
+
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i].id === contactId) {
+      contact = contacts.splice(i, 1);
+      await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    }
+  }
+
+  return contact;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  let contact = { id: nanoid(), name, email, phone };
+  contacts.push(contact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+
+  return contact;
+}
+
+module.exports = {
+  addContact,
+  removeContact,
+  getContactById,
+  listContacts,
+};
